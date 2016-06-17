880f61cd7058f7870359d1fb8b2b4435c9a1531b
greatermeans <gakouri@gmail.com>
2016-06-17 11:55:02 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, "M","Black","Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, "M","Black","Wild", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 15, "F","Black","Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, "M","Brown","Soft", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, "F","Black","Soft", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 18, "M","Green","Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 26, "F","Brown","Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "M","Black","Angry", 0);
